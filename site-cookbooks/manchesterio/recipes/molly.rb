d331fda6941c9fa35ead1c68f41365423115e26d
Chris Northwood <chris@pling.org.uk>
2013-10-13 21:55:18 +0100

    Include additional Python dependencies for Molly

--- site-cookbooks/manchesterio/recipes/molly.rb
+++ site-cookbooks/manchesterio/recipes/molly.rb
@@ -7,6 +7,14 @@ directory "#{node.manchesterio.root}/compiled_media" do
   group node.mollyproject.user
 end
 
+%w(Flask-StatsD raven[flask]).each do | python_package |
+  python_pip python_package do
+    virtualenv node.mollyproject.install_root
+    user node.mollyproject.user
+    group node.mollyproject.user
+  end
+end
+
 %w(manchesterio.conf uisettings.py).each do | config_file |
   template "#{node.manchesterio.root}/conf/#{config_file}" do
     source "#{config_file}.erb"
