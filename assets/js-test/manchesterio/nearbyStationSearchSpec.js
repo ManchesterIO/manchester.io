9efdc1242a49980718cccd1560786f63d673864e
Chris Northwood <chris@pling.org.uk>
2014-12-21 00:07:40 +0000

    Add geolocation lookup to JS

--- assets/js-test/manchesterio/nearbyStationSearchSpec.js
+++ assets/js-test/manchesterio/nearbyStationSearchSpec.js
@@ -3,10 +3,20 @@ define(['manchesterio/nearbyStationSearch'], function(NearbyStationSearch) {
 
     describe("the nearby stations button", function() {
 
-        var nearbyStationSearch, button;
+        var nearbyStationSearch, button, status, template = '/example/{lat},{lon}';
 
         beforeEach(function() {
             button = document.createElement('button');
+            button.dataset.href = template;
+            status = document.createElement('h2');
+            button.appendChild(status);
+            status.classList.add('nearby-station-search__status');
+
+            if (navigator.geolocation === undefined) {
+                navigator.geolocation = { getCurrentPosition: function() { } };
+            }
+            spyOn(navigator.geolocation, 'getCurrentPosition');
+
             nearbyStationSearch = new NearbyStationSearch(button);
         });
 
@@ -17,6 +27,43 @@ define(['manchesterio/nearbyStationSearch'], function(NearbyStationSearch) {
 
             expect(button.disabled).toBeFalsy();
         });
+
+        it("should change the status text on click", function() {
+            nearbyStationSearch.init();
+
+            button.click();
+
+            expect(status.textContent).toBe("Determining your location");
+        });
+
+        it("should change the status text on error", function() {
+            nearbyStationSearch.init();
+
+            button.click();
+            geolocationError(1);
+
+            expect(status.textContent).toBe("Your location could not be determined");
+        });
+
+        it("should redirect on success", function() {
+            nearbyStationSearch.init();
+
+            spyOn(nearbyStationSearch, '_redirect');
+            button.click();
+            geolocationSuccess(54.6, 1.23);
+
+            expect(nearbyStationSearch._redirect).toHaveBeenCalledWith('/example/54.6,1.23');
+        });
+
+        function geolocationSuccess(lat, lon) {
+            navigator.geolocation.getCurrentPosition.calls.mostRecent().args[0](
+                {coords: {latitude: lat, longitude: lon}}
+            );
+        }
+
+        function geolocationError(code) {
+            navigator.geolocation.getCurrentPosition.calls.mostRecent().args[1]({code: code});
+        }
     });
 
 });
